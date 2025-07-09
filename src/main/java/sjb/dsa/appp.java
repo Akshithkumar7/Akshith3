11c16f1948a389ce91781a095f73b439ee066d21
Akshithkumar7 <akshithk231@gmail.com>
2025-07-09 12:45:42 +0530

    done

--- src/main/java/sjb/dsa/appp.java
+++ src/main/java/sjb/dsa/appp.java
@@ -2,7 +2,7 @@ package sjb.dsa;
 
 public abstract class appp {
 	public static void main(String[] args) {
-		Stack st=new Stack(5);
+		Stack st=new Stack(5);// top=-1 0 1 2
 		st.push(10);
 		st.push(20);
 		st.push(30);
